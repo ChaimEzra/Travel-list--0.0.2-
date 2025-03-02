004900c1e81f14c3ccaf225bc6b7d5876aa431bd
ChaimEzra <chimas1357@gmail.com>
2025-03-02 12:40:57 +0200

    .

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://travel-list-0-0-2-backend.onrender.com",
 });
 
 export default api;
